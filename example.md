818d141c2b65142057f15abb423abbba0d68cee7
Katharina Sick <katharina.sick@dynatrace.com>
2023-11-01 21:32:41 +0100

    Add layout 'image-full'

--- example.md
+++ example.md
@@ -34,6 +34,12 @@ image: 'https://source.unsplash.com/collection/94734566/1920x1080'
 ---
 
 ---
+layout: image-full
+image: 'https://source.unsplash.com/collection/94734566/1920x1080'
+---
+
+
+---
 
 # Code
 
---
+++ layouts/image-full.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="slidev-layout image-full">
+      <img class="image" :src="image"/>
+    </div>
+    <Footer :event-name="eventName" :eventDate="eventDate"/>
+  </div>
+</template>
+
+<script setup>
+import Footer from "../components/Footer.vue";
+import {defineProps} from 'vue';
+
+defineProps({
+  eventName: {
+    type: String,
+  },
+  eventDate: {
+    type: String
+  },
+  image: {
+    type: String
+  }
+})
+</script>
+
+<style scoped>
+.image {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
