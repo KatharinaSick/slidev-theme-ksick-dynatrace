63ac147ba8a972a83c40bba8b5f5b9d8d0a1266c
Katharina Sick <katharina.sick@dynatrace.com>
2024-04-16 17:18:20 +0200

    fix: use catppuccin-mocha theme for code snippets

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -1,10 +1,10 @@
 import { defineShikiSetup } from '@slidev/types'
-import catppuccinMocha from 'tm-themes/themes/catppuccin-mocha.json'
 
 export default defineShikiSetup(() => {
   return {
     themes: {
-      theme: catppuccinMocha
+      dark: 'catppuccin-mocha',
+      light: 'catppuccin-mocha',
     },
   }
 })
